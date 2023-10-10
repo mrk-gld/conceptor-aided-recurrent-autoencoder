9fdcac1160e2709f00425845d97c4bd87cbbf824
Mirko Goldmann <mirko@ifisc.uib-csic.es>
2023-10-10 13:38:11 +0200

    updated run scripts

--- run_mocap_conceptor_loss.sh
+++ run_mocap_conceptor_loss.sh
@@ -1,7 +1,10 @@
 python CARAE_mocap_training.py \
     --name="mocap_conceptor_loss" \
-    --conceptor_loss_amp=0.02 \
     --num_epochs=600 \
     --steps_per_eval=25 \
-    --rnn_size=512
+    --rnn_size=512 \
+    --learning_rate=0.01 \
+    --beta_1=0.02 \
+    --beta_2=0.2 \
+    --aperture=10
 
--- run_sine_wave_conceptor_loss.sh
+++ run_sine_wave_conceptor_loss.sh
@@ -1,5 +1,8 @@
 python CARAE_sine_waves_training.py \
     --name="sine_wave_conceptor_loss" \
-    --conceptor_loss_amp=0.02 \
     --num_epochs=600 \
-    --steps_per_eval=25
+    --rnn_size=512 \
+    --steps_per_eval=25 \
+    --beta_1=0.02 \
+    --beta_2=0.2 \
+    --aperture=10
