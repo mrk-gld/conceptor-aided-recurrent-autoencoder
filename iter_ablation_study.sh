74e1a5b6168c804bbcb77cbb316b1a07f37099b1
Mirko Goldmann <mirko@ifisc.uib-csic.es>
2023-11-16 13:02:33 +0100

    update iter ablation instructs

--- iter_ablation_study.sh
+++ iter_ablation_study.sh
@@ -8,32 +8,41 @@ beta1_range=($(awk -v min=$beta1_min -v max=$beta1_max -v step=$beta1_step 'BEGI
 beta2_range=($(awk -v min=$beta1_min -v max=$beta1_max -v step=$beta1_step 'BEGIN{for (i=min; i<=max; i+=step) print 10^i}'))
 
 # Loop over the beta1 and beta2 values
-for beta1 in "${beta1_range[@]}"
+for seed in {1..5}
 do
-    for beta2 in "${beta2_range[@]}"
+    for beta1 in "${beta1_range[@]}"
     do
-        # Generate a string that contains the values of beta1 and beta2
-        beta_string="beta1_${beta1}_beta2_${beta2}"
-        echo $beta_string
-        # Run the CARAE_sine_wave0.01s_training.py script with the current beta1 and beta2 values as arguments
-        python CARAE_mocap_training.py \
-        --name=CARAE_mocap_$beta_string \
-        --beta_1=$beta1 \
-        --beta_2=$beta2 \
-        --num_epochs=102 \
-        --steps_per_eval=50 \
-        --rnn_size=512 \
-        --learning_rate=1e-2 \
-        --aperture=10
+        for beta2 in "${beta2_range[@]}"
+        do
+            # Generate a string that contains the values of beta1 and beta2
+            beta_string="beta1_${beta1}_beta2_${beta2}_seed_${seed}"
+            echo $beta_string
+            # Run the CARAE_sine_wave0.01s_training.py script with the current beta1 and beta2 values as arguments
+            python CARAE_mocap_training.py \
+            --name=CARAE_mocap_$beta_string \
+            --seed=$seed \
+            --beta_1=$beta1 \
+            --beta_2=$beta2 \
+            --plot_interp=False \
+            --calc_metric=True \
+            --num_epochs=102 \
+            --steps_per_eval=20 \
+            --rnn_size=512 \
+            --learning_rate=1e-2 \
+            --aperture=10
 
-        python CARAE_sine_waves_training.py \
-        --name=CARAE_sine_$beta_string \
-        --beta_1=$beta1 \
-        --beta_2=$beta2 \
-        --num_epochs=102 \
-        --steps_per_eval=50 \
-        --rnn_size=512 \
-        --learning_rate=1e-2 \
-        --aperture=10
+            python CARAE_sine_waves_training.py \
+            --name=CARAE_sine_$beta_string \
+            --seed=$seed \
+            --beta_1=$beta1 \
+            --beta_2=$beta2 \
+            --plot_interp=False \
+            --calc_metric=True \
+            --num_epochs=202 \
+            --steps_per_eval=40 \
+            --rnn_size=512 \
+            --learning_rate=5e-3 \
+            --aperture=10
+        done
     done
 done
