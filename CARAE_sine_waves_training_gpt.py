4a01a7f69a2f8df0f02d393d91f0a29c446676b0
Steve Abreu <s.abreu@rug.nl>
2023-11-22 14:41:49 +0100

    update gpt model for conceptor loss (and interpolation)

---
+++ CARAE_sine_waves_training_gpt.py
@@ -0,0 +1,332 @@
+import os
+from functools import partial
+from tqdm import tqdm
+from absl import flags, app
+import jax
+import jax.numpy as np
+
+from utils.rnn_utils import compute_conceptor
+# from utils.rnn_utils import forward_rnn_interp, initialize_wout, rnn_params, update
+
+from utils.utils import setup_logging_directory
+# from utils.utils import visualize_sine_interpolation, compute_JS_divergence_and_acf
+from torch.utils.tensorboard import SummaryWriter
+
+from utils.nano_gpt import GPTConfig, GPT
+
+import matplotlib.pyplot as plt
+
+# define flags
+FLAGS = flags.FLAGS
+
+
+@partial(jax.jit, static_argnames=('train',))
+def forward(state, batch, *, train: bool):
+    x, y = batch
+    rngs = {'dropout': jax.random.fold_in(jax.random.PRNGKey(0), state.step)}
+    variables = {'params': state.params}
+    y_pred, loss = state.apply_fn(variables, x, train=train, targets=y, rngs=rngs)
+    return y_pred, loss
+
+
+@partial(jax.jit, static_argnames=('train', 'aperture', 'beta_1', 'beta_2'))
+def forward_conceptor(state, batch, *, train: bool, aperture, beta_1, beta_2):
+    x, y = batch
+    rngs = {'dropout': jax.random.fold_in(jax.random.PRNGKey(0), state.step)}
+    variables = {'params': state.params}
+    y_pred, loss, info = state.apply_fn(
+        variables, x, train=train, targets=y, rngs=rngs, conceptor_loss=True,
+        aperture=aperture, beta_1=beta_1, beta_2=beta_2
+    )
+    return y_pred, loss, info
+
+
+@partial(jax.jit, static_argnames=('train', 'aperture', 'beta_1', 'beta_2'))
+def forward_conceptor_interpolation(state, batch, *, train: bool, conceptors, ratios):
+    """
+    conceptors: two conceptors of shape (2, t, features)
+                where the first one is the conceptor of the first input (min. freq.)
+                and the second one is the conceptor of the second input (max. freq.)
+    ratio: ratio of the interpolation between the two conceptors
+           if ratio = 0, the conceptor of the first input is used (min. freq.)
+           if ratio = 1, the conceptor of the second input is used (max. freq.)
+    """
+    assert not train, "forward with conceptor interpolation only works in eval mode"
+    x, y = batch
+    rngs = {'dropout': jax.random.fold_in(jax.random.PRNGKey(0), state.step)}
+    variables = {'params': state.params}
+    y_pred, loss = state.apply_fn(
+        variables, x, train=train, targets=y, rngs=rngs,
+        conceptor_interpolation=True, conceptors=conceptors, ratios=ratios
+    )
+    return y_pred, loss
+
+
+@partial(jax.jit, donate_argnames=('state',))
+def train_step(state, batch):
+    def loss_fn(params):
+        state_ = state.replace(params=params)
+        _, loss = forward(state_, batch, train=True)
+        return loss
+    grad_fn = jax.value_and_grad(loss_fn)
+    loss, grad = grad_fn(state.params)
+    state = state.apply_gradients(grads=grad)
+    return loss, state
+
+
+@partial(jax.jit, donate_argnames=('state',), static_argnames=('aperture', 'beta_1', 'beta_2'))
+def train_step_conceptor(state, batch, aperture, beta_1, beta_2):
+    def loss_fn(params):
+        state_ = state.replace(params=params)
+        _, loss, ret = forward_conceptor(state_, batch, train=True, aperture=aperture,
+                                         beta_1=beta_1, beta_2=beta_2)
+        return loss, ret
+    (loss, ret), grad = jax.value_and_grad(loss_fn, has_aux=True)(state.params)
+    state = state.apply_gradients(grads=grad)
+    return loss, state, ret
+
+
+def estimate_loss(state, train_loader, val_loader, eval_iters=10):
+    out = {}
+    for split in ['train', 'val']:
+        losses = []
+        for k, batch in enumerate(train_loader if split == 'train' else val_loader):
+            if k >= eval_iters:
+                break
+            _, loss = forward(state, batch, train=False)
+            losses.append(loss)
+        out[split] = np.array(losses).mean()
+    return out
+
+
+def sine_wave(t_pattern, s):
+    """linear interpolation between sine waves of different frequencies
+    (parametrisation of similar to Wyffels et al. (2014))"""
+    signal = []
+    for i in range(t_pattern):
+        signal.append(np.sin(0.075 * s * i))
+    return np.array(signal)
+
+
+flags.DEFINE_string("name", "sine_wave_interp", "name of this training run/experiment")
+flags.DEFINE_string("logdir", "./logs", "path to the log directory")
+flags.DEFINE_integer("num_epochs", 5000, "number of training epochs")
+# flags.DEFINE_integer("num_epochs", 3050, "number of training epochs")
+flags.DEFINE_integer("steps_per_eval", 100, "number of training steps per evaluation")
+flags.DEFINE_integer("washout", 0, "washout period")
+flags.DEFINE_integer("rnn_size", 512, "number of hidden units")
+
+flags.DEFINE_float("learning_rate", 1e-3, "learning rate")
+flags.DEFINE_float("clip_grad", 1e-2, "gradient clipping norm value")
+
+flags.DEFINE_integer("seed", 42, "seed for random number generators")
+flags.DEFINE_float("beta_1", 0.02, "conceptor loss amplitude")
+flags.DEFINE_float("beta_2", 0.01, "conceptor loss amplitude")
+flags.DEFINE_float("aperture", 10, "aperture of the conceptor")
+
+flags.DEFINE_bool("plot_interp", True, "plot interpolation between sine waves")
+flags.DEFINE_bool("calc_metric", True, "calculate metric for interpolation")
+flags.DEFINE_bool("save_param", False, "save parameters")
+
+flags.DEFINE_integer("len_cueing", 20, "length of cueing period")
+flags.DEFINE_integer("n_heads", 4, "number of attention heads")
+flags.DEFINE_integer("n_layers", 3, "number of transformer layers")
+# flags.DEFINE_integer("n_layers", 1, "number of transformer layers")
+flags.DEFINE_integer("n_embd", 128, "embedding dimension")
+
+flags.DEFINE_boolean("conceptor_loss", True, "use conceptor loss")
+
+
+def setup_model(t_pattern=300):
+    # initialize GPT model
+    config = GPTConfig(
+        block_size=t_pattern,  # max sequence length (was 512)
+        n_layer=FLAGS.n_layers,
+        n_head=FLAGS.n_heads,  # was 8
+        n_embd=FLAGS.n_embd,  # embedding dimension (was 512)
+        dropout=0.,
+        input_dim=1,
+        # vocab_size=50257,  # vocab size (not used here)
+    )
+    model = GPT(config)
+    state = model.create_state(
+        learning_rate=6e-4,
+        weight_decay=0.0,
+        beta1=0.9,
+        beta2=0.95,
+        decay_lr=True,
+        warmup_iters=FLAGS.num_epochs // 10,  # was 2000
+        lr_decay_iters=FLAGS.num_epochs,
+        min_lr=1e-5,  # was 6e-5
+        params=None,
+    )
+    return config, model, state
+
+
+def main(_):
+    # setup dataset: 2 training sine waves, 8 testing sine waves
+    t_pattern = 300
+    datasets = jax.vmap(sine_wave, in_axes=(None, 0))(t_pattern, np.linspace(1, 3, 10))
+    datasets = np.expand_dims(datasets, axis=2)
+    data_sample = np.array([0, -1])
+    ut_train = datasets[data_sample, 0:datasets.shape[1] - 1, :]
+    yt_train = datasets[data_sample, 1:datasets.shape[1], :]
+    ut_test = datasets[1:-1, 0:datasets.shape[1] - 1, :]
+    yt_test = datasets[1:-1, 1:datasets.shape[1], :]
+
+    log_folder = setup_logging_directory(FLAGS.logdir, FLAGS.name)
+    tb_writer = SummaryWriter(log_dir=log_folder)
+    os.makedirs(os.path.join(log_folder, "ckpt"), exist_ok=True)
+    os.makedirs(os.path.join(log_folder, "plots"), exist_ok=True)
+
+    with open(os.path.join(log_folder, "flags.txt"), "w") as f:
+        f.write(FLAGS.flags_into_string())
+
+    config, model, state = setup_model(t_pattern=t_pattern)
+
+    for epoch_idx in (pbar := tqdm(range(FLAGS.num_epochs))):
+        pbar.set_description("Training")
+        shuffled_idxs = jax.random.permutation(jax.random.PRNGKey(epoch_idx), np.arange(2))
+        batch = (ut_train[shuffled_idxs], yt_train[shuffled_idxs])
+
+        ############################
+        # training
+        ############################
+
+        if FLAGS.conceptor_loss:
+            loss, state, info = train_step_conceptor(
+                state, batch, aperture=FLAGS.aperture, beta_1=FLAGS.beta_1, beta_2=FLAGS.beta_2
+            )
+            loss_c, loss_y, err_c, err_c_mean, X = info
+            # C = compute_conceptor(X, FLAGS.aperture)
+            tb_writer.add_scalar("loss", loss.item(), epoch_idx)
+            tb_writer.add_scalar("loss_c", loss_c.item(), epoch_idx)
+            tb_writer.add_scalar("loss_y", loss_y.item(), epoch_idx)
+            tb_writer.add_scalar("loss_c_wo_mean", err_c.item(), epoch_idx)
+            tb_writer.add_scalar("loss_c_mean", err_c_mean.item(), epoch_idx)
+            # NOTE: get grad norms?
+
+        else:
+            loss, state = train_step(state, batch)
+            tb_writer.add_scalar("loss", loss.item(), epoch_idx)
+
+        ############################
+        # evaluation
+        ############################
+
+        if epoch_idx % FLAGS.steps_per_eval == 0:
+            # evaluate on train and test (loss only)
+            conceptor_manifold = jax.vmap(lambda x: compute_conceptor(x, FLAGS.aperture))(X)
+            if FLAGS.conceptor_loss:
+                # compute train loss with the conceptor plugged in
+                _, train_loss = forward_conceptor_interpolation(
+                    state, batch, train=False, conceptors=conceptor_manifold,
+                    ratios=np.array([0., 1.])
+                )
+                # compute test loss with the conceptor plugged in
+                _, test_loss = forward_conceptor_interpolation(
+                    state, (ut_test, yt_test), train=False, conceptors=conceptor_manifold,
+                    ratios=np.linspace(0, 1, 10)[1:-1]
+                )
+            else:
+                _, train_loss = forward(state, batch, train=False)
+                _, test_loss = forward(state, (ut_test, yt_test), train=False)
+
+            tb_writer.add_scalar("train_loss", train_loss.item(), epoch_idx)
+            tb_writer.add_scalar("test_loss", test_loss.item(), epoch_idx)
+
+            # generative mode for unseen testing sine wave frequencies
+            gen_key = jax.random.PRNGKey(epoch_idx)
+            pred = model.generate(
+                key=gen_key,
+                params=state.params,
+                x=ut_test[:, :FLAGS.len_cueing, :],
+                max_new_tokens=config.block_size-FLAGS.len_cueing,
+            )
+            auto_loss_test = np.mean(
+                (pred[:, FLAGS.len_cueing:-1, :] - ut_test[:, FLAGS.len_cueing:, :]) ** 2
+            )
+            tb_writer.add_scalar("auto_loss_test", auto_loss_test.item(), epoch_idx)
+
+            # generative mode (on training)
+            gen_key = jax.random.PRNGKey(epoch_idx)
+            pred = model.generate(
+                key=gen_key,
+                params=state.params,
+                x=ut_train[:, :FLAGS.len_cueing, :],
+                max_new_tokens=config.block_size-FLAGS.len_cueing,
+            )
+            auto_loss = np.mean(
+                (pred[:, FLAGS.len_cueing:-1, :] - ut_train[:, FLAGS.len_cueing:, :]) ** 2
+            )
+            tb_writer.add_scalar("auto_loss", auto_loss.item(), epoch_idx)
+
+            # plot generative mode output
+            fig, axs = plt.subplots(2, 1, sharey=True, sharex=True, figsize=(10, 5), dpi=300)
+            for i in range(2):
+                axs[i].plot(np.arange(t_pattern), pred[i, :, 0], label="generated")
+                axs[i].plot(np.arange(t_pattern-1), ut_train[i, :, 0], label="cueing")
+                axs[i].vlines(FLAGS.len_cueing, -1, 1, color="k", linestyle="--", label="cue end")
+                axs[i].legend()
+            fig.savefig(f"{log_folder}/plots/generative_{epoch_idx:03}.png")
+            plt.close(fig)
+
+            # save parameters
+            np.save(f"{log_folder}/ckpt/params_{epoch_idx+1:03}.npy", state.params)
+
+            # save conceptor
+            C = jax.vmap(lambda x: compute_conceptor(x, FLAGS.aperture))(X)
+            conceptor = {"C_1": C[0], "C_2": C[1]}
+            np.savez(
+                f"{log_folder}/ckpt/conceptor_{epoch_idx+1:03}.npz",
+                **{key: np.array(conceptor[key]) for key in conceptor.keys()},
+            )
+
+            # update progress bar
+            pbar.set_postfix_str(f"loss train={train_loss:.2f}, test={test_loss:.2f}")
+
+        # if epoch_idx % FLAGS.steps_per_eval == 0:
+        #     f_partial = partial(compute_conceptor, aperture=FLAGS.aperture, svd=True)
+        #     C = jax.vmap(f_partial)(X[:, FLAGS.washout:, :])
+
+        #     if FLAGS.calc_metric:
+        #         lamda = 0.5
+        #         len_seqs = 1000
+
+        #         _, y_interp = forward_rnn_interp(
+        #             params_rnn,
+        #             C,
+        #             x_init=None,
+        #             ratio=lamda,
+        #             length=len_seqs,
+        #             spd_interp=None,
+        #         )
+
+        #         js_div1, js_div2, acf1, acf2 = compute_JS_divergence_and_acf(
+        #             ut_train[0], ut_train[1], y_interp, lag=30, bins=50
+        #         )
+
+        #         metric = 0.25 * (js_div1 + js_div2 + acf1 + acf2)
+        #         tb_writer.add_scalar("metric", metric, epoch_idx)
+
+        #     if FLAGS.plot_interp:
+        #         visualize_sine_interpolation(
+        #             params_rnn, C, log_folder, f"{epoch_idx:03}"
+        #         )
+        #     if FLAGS.save_param:
+        #         # save params
+        #         np.savez(
+        #             f"{log_folder}/ckpt/params_{epoch_idx+1:03}.npz",
+        #             **{key: params_rnn[key].__array__() for key in params_rnn.keys()},
+        #         )
+
+        #         conceptor = {"C_1": C[0], "C_2": C[1]}
+        #         # save params
+        #         np.savez(
+        #             f"{log_folder}/ckpt/conceptor_{epoch_idx+1:03}.npz",
+        #             **{key: np.array(conceptor[key]) for key in conceptor.keys()},
+        #         )
+
+
+if __name__ == "__main__":
+    app.run(main)
--- utils/nano_gpt.py
+++ utils/nano_gpt.py
@@ -34,6 +34,27 @@ class GPTConfig:
     input_dim: int = 1
 
 
+def compute_conceptor(X, aperture, svd=False):
+    """
+    Computes the conceptor matrix for a given input matrix X and an aperture value.
+
+    Parameters:
+    X (numpy.ndarray): Input matrix of shape (n_samples, n_features).
+    aperture (float): Aperture value used to compute the conceptor matrix.
+    svd (bool): if true compute conceptor using singular value decomposition
+    Returns:
+    numpy.ndarray: Conceptor matrix of shape (n_features, n_features).
+    """
+    R = jnp.dot(X.T, X) / X.shape[0]
+    if not svd:
+        C = jnp.dot(R, jnp.linalg.inv(R + aperture ** (-2) * jnp.eye(R.shape[0])))
+        return C
+    else:
+        U, S, _ = jnp.linalg.svd(R, full_matrices=False, hermitian=True)
+        C = U * (S / (S + 0.001 * jnp.ones(S.shape))) @ U.T
+        return C
+
+
 class CausalSelfAttention(nn.Module):
     config: GPTConfig
 
@@ -133,12 +154,29 @@ class GPT(nn.Module):
         self.dense_out = nn.Dense(config.input_dim)
 
     def __call__(
-        self, x: jax.Array, *, train: bool, targets: Optional[jax.Array] = None
+        self, x: jax.Array, *, train: bool, targets: Optional[jax.Array] = None,
+        conceptor_loss: bool = False, aperture: float = 10, beta_1=0, beta_2=0,
+        conceptor_interpolation: bool = False,  conceptors: Optional[jax.Array] = None,
+        ratios: Optional[jax.Array] = None,
     ):
         """
         x: inputs of shape (b, t, features)
         targets: targets of shape (b, t, features)
+
+        conceptor_interpolation: if true, interpolate between two conceptors (see conceptors arg)
+        conceptors: two conceptors of shape (2, t, features)
+                    where the first one is the conceptor of the first input (min. freq.)
+                    and the second one is the conceptor of the second input (max. freq.)
+        ratio: ratio of the interpolation between the two conceptors
+               if ratio = 0, the conceptor of the first input is used (min. freq.)
+               if ratio = 1, the conceptor of the second input is used (max. freq.)
         """
+        if conceptor_interpolation:
+            # assert x.shape[0] == 1, "batch size must be 1 for interpolation"
+            assert ratios.shape[0] == x.shape[0], "ratio size mismatch"
+            assert conceptors is not None, "conceptor must be provided for interpolation"
+            assert not conceptor_loss, "conceptor loss not supported for interpolation"
+
         b, t, f = x.shape
         assert (
             t <= self.config.block_size
@@ -151,23 +189,56 @@ class GPT(nn.Module):
         pos_emb = self.wpe(pos)  # position embeddings of shape (1, t, n_embd)
         x = self.drop(tok_emb + pos_emb, deterministic=not train)
 
-        # forward the GPT model itself
-        for block in self.h:
+        if conceptor_interpolation:
+            # interpolate between the two conceptors at the given ratio
+            # conceptor -> (batch, time, n_layers*features)
+            conceptor = jax.vmap(lambda c, r: (1-r)*c[0] + r*c[1], (None, 0))(conceptors, ratios)
+            # conceptor = conceptors[0] * (1 - ratio) + conceptors[1] * ratio
+
+        X = []
+        for idx, block in enumerate(self.h):
+            # forward the GPT model itself
             x = block(x, train=train)
+            if conceptor_loss:
+                # save the hidden states for the conceptor computation later
+                X.append(x)
+            if conceptor_interpolation:
+                # plug in the conceptor matrix
+                # TODO: make this a batched matrix multiplication
+                c_idx = jnp.split(jnp.split(conceptor, 3, axis=1)[idx], 3, axis=2)[idx]
+                x = jax.vmap(lambda ci, xi: (ci @ xi.T).T)(c_idx, x)
+
+        # layer norm
         x = self.ln_f(x)
 
+        if conceptor_loss:
+            # X -> (batch, time, features*layers)
+            X = jnp.concatenate(X, axis=2)
+            C = jax.vmap(lambda x: compute_conceptor(x, aperture))(X)
+            M = jax.vmap(lambda x: jnp.mean(x, axis=0), (0))(X)
+            # X = jnp.stack(X, axis=0)
+
         # logits = self.wte.attend(x)
-        # TODO: check if this is correct - need to apply transposed wte_raw matrix?
+        # NOTE: rather apply transposed wte_raw matrix?
         logits = self.dense_out(x)
 
-        if targets is not None:
+        if targets is not None and not conceptor_loss:
             # if we are given some desired targets also calculate the loss
             # loss = optax.softmax_cross_entropy_with_integer_labels(logits, targets).mean()
             loss = optax.l2_loss(logits, targets).mean()
-        else:
-            loss = None
+            return logits, loss
 
-        return logits, loss
+        elif targets is not None and conceptor_loss:
+            loss_rec = optax.l2_loss(logits, targets).mean()
+            err_c = jnp.linalg.norm(C[0] - C[1])
+            err_c_mean = jnp.linalg.norm(M[0] - M[1])
+            loss_conceptor = beta_1 * err_c + beta_2 * err_c_mean
+            # ridge = np.linalg.norm(params["wout"] ** 2) + np.linalg.norm(params["w"] ** 2)
+            loss = loss_rec + loss_conceptor  # + 0.01 * ridge
+            return logits, loss, (loss_conceptor, loss_rec, err_c, err_c_mean, X)
+
+        else:
+            return logits, None
 
     def crop_block_size(self, params, block_size: int):
         # model surgery to decrease the block size if necessary
@@ -193,28 +264,30 @@ class GPT(nn.Module):
         embedding weights). We are then returning the optax optimizer object.
         """
 
-        def get_optimizer(decay):
-            return optax.adamw(
-                learning_rate=learning_rate,
-                b1=betas[0],
-                b2=betas[1],
-                weight_decay=decay,
-            )
+        assert weight_decay == 0. or weight_decay is None, "weight decay not supported atm"
 
-        def partition_fn(path: Tuple[str, ...], x) -> str:
-            if path[-1] in ("bias", "scale", "embedding"):
-                return "no_decay"
-            elif path[-1] in ("kernel",):
-                return "decay"
-            else:
-                raise ValueError(f"Unrecognized parameter: {path}")
+        # def get_optimizer(decay):
+        #     return optax.adamw(
+        #         learning_rate=learning_rate,
+        #         b1=betas[0],
+        #         b2=betas[1],
+        #         weight_decay=decay,
+        #     )
 
-        partition_optimizers = {
-            "decay": get_optimizer(weight_decay),
-            "no_decay": get_optimizer(0.0),
-        }
-        param_partitions = freeze(path_aware_map(partition_fn, params))
-        tx = optax.multi_transform(partition_optimizers, param_partitions)
+        # def partition_fn(path: Tuple[str, ...], x) -> str:
+        #     if path[-1] in ("bias", "scale", "embedding"):
+        #         return "no_decay"
+        #     elif path[-1] in ("kernel",):
+        #         return "decay"
+        #     else:
+        #         raise ValueError(f"Unrecognized parameter: {path}")
+
+        # partition_optimizers = {
+        #     "decay": get_optimizer(weight_decay),
+        #     "no_decay": get_optimizer(0.0),
+        # }
+        # param_partitions = freeze(path_aware_map(partition_fn, params))
+        # tx = optax.multi_transform(partition_optimizers, param_partitions)
 
         tx = optax.adamw(
             learning_rate=learning_rate,
@@ -226,47 +299,26 @@ class GPT(nn.Module):
         return tx
 
     def generate(
-        self, key, params, input_tokens, max_new_tokens, temperature=1.0, top_k=None
+        self, key, params, x: jax.Array, max_new_tokens: int,  # temperature=1.0, top_k=None
     ):
         """
         Take a conditioning sequence of indices idx (LongTensor of shape (b,t)) and complete
         the sequence max_new_tokens times, feeding the predictions back into the model each time.
         Most likely you'll want to make sure to be in model.eval() mode of operation for this.
         """
-        B, T = input_tokens.shape
-        padding = jnp.zeros((B, max_new_tokens), dtype=jnp.int32)
-        tokens = jnp.concatenate([input_tokens, padding], axis=-1)
+        B, T, F = x.shape
+        padding = jnp.zeros((B, max_new_tokens, F), dtype=x.dtype)
+        tokens = jnp.concatenate([x, padding], axis=1)
         indexes = jnp.arange(T, T + max_new_tokens)
 
         # tokens index -> tokens None
         def scan_f(tokens, i):
-            # l: x y
-            # t: a b - -
-            # i: 0 1 2 3
-            step_key = jax.random.fold_in(key, i)
-            # if the sequence context is growing too long we must crop it at block_size
-            # idx_cond = idx if idx.size(1) <= self.config.block_size
-            #   else idx[:, -self.config.block_size:]
             # forward the model to get the logits for the index in the sequence
-            logits, _ = self.apply({"params": params}, tokens, train=False)
+            y_pred, _ = self.apply({"params": params}, tokens, train=False)
             # pluck the logits at the final step and scale by desired temperature
-            logits = logits[:, i - 1, :] / temperature
-            # optionally crop the logits to only the top k options
-            # sample from the distribution
-            if top_k is not None:
-                top_logits, top_tokens = jax.lax.top_k(
-                    logits, min(top_k, logits.shape[-1])
-                )
-                token_idx = jax.random.categorical(step_key, top_logits, axis=-1)
-                next_token = jnp.take_along_axis(
-                    top_tokens, token_idx[:, None], axis=-1
-                ).squeeze(-1)
-            else:
-                next_token = jax.random.categorical(step_key, logits, axis=-1)
-                # logits = jnp.where(logits < v[:, -1:], float('-inf'), logits)
+            y_pred = y_pred[:, i - 1, :]
             # append sampled index to the running sequence and continue
-            tokens = tokens.at[:, i].set(next_token)
-
+            tokens = tokens.at[:, i].set(y_pred)
             return tokens, None
 
         tokens, _ = jax.lax.scan(scan_f, tokens, indexes)
