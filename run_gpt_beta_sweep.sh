f4ab5133ee5a8a6538c29613e5fab239a23938fe
Steve Abreu <s.abreu@rug.nl>
2023-11-23 09:14:37 +0100

    change script

--- run_gpt_beta_sweep.sh
+++ run_gpt_beta_sweep.sh
@@ -6,8 +6,7 @@ for beta1 in "${values[@]}"; do
     # Replace the following line with your command, using $value as needed
     for beta2 in "${values[@]}"; do
         echo "Running command with beta1=$beta1, beta2=$beta2"
-        beta1str="${beta1#0.}"
-        beta2str="${beta2#0.}"
-        python CARAE_sine_waves_training_gpt.py --name=gpt_CARAE_$beta1str_$beta2str --num_epochs=10000 --n_layers=3 --n_heads=4 --conceptor_loss --steps_per_eval=200 --conceptor_layers=1,2 --len_cueing=20 --beta_1=$beta1 --beta_2=$beta2
+        betastr="${beta1#0.}${beta2#0.}"
+        python CARAE_sine_waves_training_gpt.py --name=gpt_CARAE_$betastr --num_epochs=5000 --n_layers=3 --n_heads=4 --conceptor_loss --steps_per_eval=200 --conceptor_layers=1,2 --len_cueing=20 --beta_1=$beta1 --beta_2=$beta2
     done
 done
